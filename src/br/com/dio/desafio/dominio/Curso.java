d509518b3b84ef8a7fe0fe6be2669a1b42823808
GuilhermeSalvador84 <guilhermesalvador@hotmail.com>
2023-01-13 11:20:56 -0300

    final

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,43 +1,32 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
-
-    public String getTitulo() {
-        return titulo;
-    }
+public class Curso extends Conteudo{
 
     public Curso() {
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+    private int cargaHoraria;
 
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 
     public int getCargaHoraria() {
         return cargaHoraria;
     }
 
+
+
     public void setCargaHoraria(int cargaHoraria) {
         this.cargaHoraria = cargaHoraria;
     }
 
-    private int cargaHoraria;
-
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
